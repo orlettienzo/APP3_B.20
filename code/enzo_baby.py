6696eae9468c9093e852e36dd495f19fa9422246
orlettienzo <enzo.andrade@student.uclouvain.be>
2024-10-28 21:47:39 +0100

    Add milk consumed quantity

--- code/enzo_baby.py
+++ code/enzo_baby.py
@@ -1,17 +1,14 @@
 from microbit import *
-
-#1. Definition de l'image initiale pour le be:bi enfant
-baby_image = Image("99990:"     
-                "90000:"        
-                "99900:"        
-                "90000:"        
-                "99990:")
-
-
-display.show(baby_image) # image initale = lettre "E"
-
-from microbit import *
 import radio
+
+# 1. Définition de l'image initiale pour le bébé enfant
+baby_image = Image("99990:"
+                   "90000:"
+                   "99900:"
+                   "90000:"
+                   "99990:")
+
+display.show(baby_image)  # image initiale = lettre "E"
 
 # Active le module radio
 radio.on()
@@ -23,18 +20,26 @@ radio.config(channel=7)
 sleeping = True
 radio.send("sleeping")
 
+# Variable pour la quantité de lait consommée
+milk_consumed = 0
+
+# Envoie la quantité de lait consommée via radio
+radio.send("milk consumed: {} ml".format(milk_consumed))
+
 while True:
     # Vérifie le mouvement à l'aide de l'accéléromètre
     movement = accelerometer.current_gesture()
-    
+
     if movement == "shake" or movement == "freefall" or movement == "move":
         if sleeping:
             # Envoie le message "awake" si l'appareil dormait et qu'il y a eu un mouvement
             radio.send("awake")
             sleeping = False
-    
-        
-    
+
+    # Vérifie si le bouton A est pressé pour afficher la quantité de lait
+    if button_a.is_pressed():
+        display.scroll("Milk: {} ml".format(milk_consumed))
+
     # Pause pour réduire la fréquence de vérification
     sleep(500)
 
