ab6a3b0f47fc611bdc7408258b1fb36f319d6ba2
orlettienzo <enzo.andrade@student.uclouvain.be>
2024-10-29 15:57:15 +0100

    Enfant: receive + drink milk + send new milk_consumed value

--- code/enzo_baby.py
+++ code/enzo_baby.py
@@ -1,14 +1,18 @@
 from microbit import *
 import radio
+import music
 
-# 1. Définition de l'image initiale pour le bébé enfant
-baby_image = Image("99990:"
-                   "90000:"
-                   "99900:"
-                   "90000:"
-                   "99990:")
 
-display.show(baby_image)  # image initiale = lettre "E"
+def show_image():
+    # 1. Définition de l'image initiale pour le bébé enfant
+    baby_image = Image("99990:"
+                       "90000:"
+                       "99900:"
+                       "90000:"
+                       "99990:")
+
+    display.show(baby_image)  # image initiale = lettre "E"
+
 
 # Active le module radio
 radio.on()
@@ -23,16 +27,24 @@ radio.send("sleeping")
 # Variable pour la quantité de lait consommée
 milk_consumed = 0
 
+
+def drink_milk(milk_consumed, dose):
+    milk_consumed += dose
+
+
 while True:
+    show_image()
+
     # Vérifie le mouvement à l'aide de l'accéléromètre
     movement = accelerometer.current_gesture()
     message = radio.receive()
     m = ''
 
     if movement == "shake" or movement == "freefall" or movement == "move":
-            # Envoie le message "awake" si l'appareil dormait et qu'il y a eu un mouvement
-            radio.send("awake")
-            sleeping = False
+        # Envoie le message "awake" si l'appareil dormait et qu'il y a eu un mouvement
+        radio.send("awake")
+        sleep(1000)  # Max 1 message/ seconde
+        sleeping = False
 
     if message:
         m = message.strip()
@@ -40,4 +52,15 @@ while True:
     if m == 'get_milk':
         radio.send(str(milk_consumed))
         display.scroll("{} ml".format(milk_consumed))
+
+    elif m.isdigit():
+        dose = int(m)
+        milk_consumed += dose  # L'enfant est en train de boire la dose de lait
+        display.show(Image.HAPPY)  # :)
+        music.play(music.POWER_UP)
+        radio.send("{}".format(milk_consumed))
+        display.scroll("{}".format(milk_consumed))
+
+    else:
+        sleep(500)
 
