bf64ea3b4b1a8c485f70cf53dad97dbfcb3776ad
Jad <jadmrabti2005@gmail.com>
2024-11-18 12:14:53 +0100

    free fall

--- code/baby_behavior.py
+++ code/baby_behavior.py
@@ -1,6 +1,5 @@
 from microbit import *
 import radio
-import random
 import music
 
 # Active le module radio
@@ -8,7 +7,7 @@ radio.on()
 radio.config(channel=7)
 
 
-# creer portefeuille de l'enfant
+# Classe représentant le portefeuille numérique de l'enfant
 class DigitalWallet:
     def __init__(self, titulaire, numero_compte):
         self.titulaire = titulaire
@@ -19,161 +18,128 @@ class DigitalWallet:
         self.solde += valeur
 
 
-# Cette fonction sera utiliser plus tard
-# def calculer_appreciation(valeur_ancienne, valeur_nouvelle):
-# if valeur_ancienne == 0:
-# raise ValueError("La valeur ancienne ne peut pas être zéro pour calculer l'appréciation en pourcentage.")
-
-# appreciation = ((valeur_nouvelle - valeur_ancienne) / valeur_ancienne) * 100
-# return appreciation
-
+# Initialisation du portefeuille
 baby_wallet = DigitalWallet('Micro Enfant', 'BE00 0001')
 
-getting_btc = False
-while not getting_btc:
-    message = radio.receive()
-    if message:
-        amount = float(message)
 
-        baby_wallet.receive(amount)
-
-        display.scroll('{} BTC'.format(baby_wallet.solde))
-
-        getting_btc = True
-
-    else:
-        sleep(1000)
-
-
+# Fonction pour afficher une image d'enfant
 def show_image():
-    # 1. Définition de l'image initiale pour le bébé enfant
     baby_image = Image("99990:"
                        "90000:"
                        "99900:"
                        "90000:"
                        "99990:")
+    display.show(baby_image)
 
-    display.show(baby_image)  # image initiale = lettre "E"
 
-
+# Fonction pour vérifier le niveau d'agitation
 def check_agitation():
-    # Capture les valeurs de l'accéléromètre sur les axes X, Y et Z
     agitation = accelerometer.get_strength()
-    limite = 3000
-
-    # Définir les niveaux d'agitation (ajuster si nécessaire)
     if agitation < 1000:
         return "agitation faible"
-    elif agitation < 2999:
+    elif agitation < 3000:
         return "agitation moyenne"
-    elif agitation >= limite:
+    else:
         return "agitation elevee"
 
 
+# Fonction pour envoyer un message sur l'agitation
 def send_agitation():
     message = check_agitation()
-    radio.send(str(message))
+    radio.send(message)
 
 
-# Active le module radio
-radio.on()
-
-# Configure le canal radio sur 7 (vous pouvez choisir un autre si nécessaire)
-radio.config(channel=7)
-
-# Variable pour stocker l'état précédent
-sleeping = True
-calm = True
-radio.send("sleeping")
-
-# Variable pour la quantité de lait consommée
-milk_consumed = 0
-
-
+# Fonction pour afficher la quantité de lait consommée
 def show_milk(milk_consumed):
-    display.scroll(str(milk_consumed) + "ml")
+    display.scroll(str((milk_consumed) + "ml"))
 
 
+# Gestion de la consommation de lait
 def drink_milk(milk_consumed, dose):
     milk_consumed += dose
+    display.scroll(str(milk_consumed) + "ml")
+    return milk_consumed
 
 
-communication = True
-while communication:
-    # display.scroll(password)
+# Variables globales
+sleeping = True
+calm = True
+milk_consumed = 0
+
+# Boucle principale
+while True:
+    # Affiche une image de l'enfant
     show_image()
+
+    # Si le bouton A est pressé, affiche la quantité de lait consommée
     if button_a.was_pressed():
         show_milk(milk_consumed)
-    # Vérifie le mouvement à l'aide de l'accéléromètre
+
+    # Vérifie les mouvements avec l'accéléromètre
     movement = accelerometer.current_gesture()
-    message = radio.receive()
-    m = ''
 
-    etat_enfant = check_agitation()
-
-    # Verification si l'enfant tombe
+    # Vérifie si l'enfant est en chute libre
     if movement == "freefall":
         radio.send("agitation elevee")
-        sleep(1000)  # Max 1 message/ seconde
         sleeping = False
         calm = False
         while not calm:
             display.show(Image.CONFUSED)
             message = radio.receive()
-            if message:
-                if message == "calm":
-                    display.show(Image.HAPPY)
-                    music.play(music.POWER_UP)
-                    calm = True
-            sleep(1000)
+            if message == "calm":
+                display.show(Image.HAPPY)
+                music.play(music.POWER_UP)
+                calm = True
+            sleep(100)
 
+    # Vérifie le niveau d'agitation
+    etat_enfant = check_agitation()
     if etat_enfant == "agitation elevee":
-        # Envoie le message "awake" si l'appareil dormait et qu'il y a eu un mouvement
         send_agitation()
-        sleep(1000)  # Max 1 message/ seconde
         sleeping = False
         calm = False
         while not calm:
             display.show(Image.CONFUSED)
             message = radio.receive()
-            if message:
-                if message == "calm":
-                    display.show(Image.HAPPY)
-                    music.play(music.POWER_UP)
-                    calm = True
-            sleep(1000)
+            if message == "calm":
+                display.show(Image.HAPPY)
+                music.play(music.POWER_UP)
+                calm = True
+            sleep(100)
 
+    # Réception des messages via radio
+    message = radio.receive()
     if message:
-        m = message.strip()
+        message = message.strip()
 
-    if m == 'get_milk':
-        radio.send(str(milk_consumed))
-        display.scroll("{} ml".format(milk_consumed))
+        # Gérer la demande de lait consommé
+        if message == "get_milk":
+            radio.send(str(milk_consumed) + "ml")
+            display.scroll(str(milk_consumed) + "ml")
 
-    if m == 'get_temperature':
-        temp = temperature()
-        radio.send(str(temp) + " C")
-        if temp < 35 or temp > 37:
-            happy = False
-            while not happy:
-                display.show(Image.SAD)
-                message = radio.receive()
-                if message == 'medicament':
-                    display.show(Image.HAPPY)
-                    music.play(music.POWER_UP)
-                    radio.send('merci')
-                    happy = True
-                else:
-                    sleep(1000)
+        # Gérer la demande de température
+        elif message == "get_temperature":
+            temp = temperature()
+            radio.send(str(temp) + "C")
+            if temp < 35 or temp > 37:
+                happy = False
+                while not happy:
+                    display.show(Image.SAD)
+                    response = radio.receive()
+                    if response == "medicament":
+                        display.show(Image.HAPPY)
+                        music.play(music.POWER_UP)
+                        radio.send("merci")
+                        happy = True
+                    sleep(100)
 
+        # Gérer la consommation de lait
+        elif message.isdigit():
+            dose = int(message)
+            milk_consumed = drink_milk(milk_consumed, dose)
+            display.show(Image.HAPPY)
+            music.play(music.POWER_UP)
+            radio.send(str(milk_consumed) + "ml")
 
-    elif m.isdigit():
-        dose = int(m)
-        milk_consumed += dose  # L'enfant est en train de boire la dose de lait
-        display.show(Image.HAPPY)  # :)
-        music.play(music.POWER_UP)
-        radio.send("{}".format(milk_consumed))
-        display.scroll("{} ml".format(milk_consumed))
-
-    else:
-        sleep(500)
+        else:
+            sleep(500)
