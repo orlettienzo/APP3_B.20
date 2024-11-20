9e8897f768aa1bbb6d114d022f716bddd590fecd
Enzo Andrade Orletti <enzo.andrade@student.uclouvain.be>
2024-11-20 17:56:13 +0100

    Messages TLV : 1st try

---
+++ cryptographie/enzoTLV.py
@@ -0,0 +1,39 @@
+import baby_functions as b
+
+
+#Exemple de transformation de message en TLV
+
+"""
+TLV = Type Lenght Value
+"""
+import random
+
+message = "Hello"
+m = message.strip().lower()
+
+def tlv_simple(message):
+    type = random.randint(1, 10)
+    length = len(message)
+    message = message
+
+    return (type, length, message)
+
+new = tlv_simple(message)
+print(new)
+
+def tlv_en_bits(tupla):
+    """
+    tupla == (type = int, length = int, message = str)
+    """
+    if not isinstance(tupla[0], int) or not isinstance(tupla[1], int) or not isinstance(tupla[2], str):
+        return False
+
+    to_send = ''
+    to_send += str(tupla[0]) + "|"
+    to_send += str(tupla[1]) + "|"
+    to_send += b.hashing(tupla[2])
+
+    return to_send
+
+to_send = tlv_en_bits(new)
+print(to_send)
