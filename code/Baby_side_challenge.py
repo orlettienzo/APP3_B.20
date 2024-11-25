d5ad8bd7397274a82dc4c5f21d4e20fd5712a524
Enzo Andrade Orletti <enzo.andrade@student.uclouvain.be>
2024-11-25 23:05:17 +0100

    Etablissement de la connexion (v1.0)

--- code/Baby_side_challenge.py
+++ code/Baby_side_challenge.py
@@ -1,68 +1,51 @@
-#from microbit import *
-#import radio
-#import random
-#import music
-#from baby_behavior import *
+from microbit import *
+import radio
+import random
+import music
 
-#Can be used to filter the communication, only the ones with the same parameters will receive messages
-#radio.config(group=23, channel=2, address=0x11111111)
-#default : channel=7 (0-83), address = 0x75626974, group = 0 (0-255)
+
+# Can be used to filter the communication, only the ones with the same parameters will receive messages
+# radio.config(group=23, channel=2, address=0x11111111)
+# default : channel=7 (0-83), address = 0x75626974, group = 0 (0-255)
 
 
 def hashing(string):
-	"""
-	Hachage d'une chaîne de caractères fournie en paramètre.
-	Le résultat est une chaîne de caractères.
-	Attention : cette technique de hachage n'est pas suffisante (hachage dit cryptographique) pour une utilisation en dehors du cours.
+    """
+    Hachage d'une chaîne de caractères fournie en paramètre.
+    Le résultat est une chaîne de caractères.
+    Attention : cette technique de hachage n'est pas suffisante (hachage dit cryptographique) pour une utilisation en dehors du cours.
 
-	:param (str) string: la chaîne de caractères à hacher
-	:return (str): le résultat du hachage
-	"""
-	def to_32(value):
-		"""
-		Fonction interne utilisée par hashing.
-		Convertit une valeur en un entier signé de 32 bits.
-		Si 'value' est un entier plus grand que 2 ** 31, il sera tronqué.
+    :param (str) string: la chaîne de caractères à hacher
+    :return (str): le résultat du hachage
+    """
 
-		:param (int) value: valeur du caractère transformé par la valeur de hachage de cette itération
-		:return (int): entier signé de 32 bits représentant 'value'
-		"""
-		value = value % (2 ** 32)
-		if value >= 2**31:
-			value = value - 2 ** 32
-		value = int(value)
-		return value
+    def to_32(value):
+        """
+        Fonction interne utilisée par hashing.
+        Convertit une valeur en un entier signé de 32 bits.
+        Si 'value' est un entier plus grand que 2 ** 31, il sera tronqué.
 
-	if string:
-		x = ord(string[0]) << 7
-		m = 1000003
-		for c in string:
-			x = to_32((x*m) ^ ord(c))
-		x ^= len(string)
-		if x == -1:
-			x = -2
-		return str(x)
-	return ""
+        :param (int) value: valeur du caractère transformé par la valeur de hachage de cette itération
+        :return (int): entier signé de 32 bits représentant 'value'
+        """
+        value = value % (2 ** 32)
+        if value >= 2 ** 31:
+            value = value - 2 ** 32
+        value = int(value)
+        return value
 
-"""
---------------------
-Ajout fonction TLV()
---------------------
-"""
-def tlv(type, message):
-    lenght = len(message)
-    message = message.strip().lower()
-    tlv = "{}|{}|{}".format(type, lenght, message)
-    return tlv
+    if string:
+        x = ord(string[0]) << 7
+        m = 1000003
+        for c in string:
+            x = to_32((x * m) ^ ord(c))
+        x ^= len(string)
+        if x == -1:
+            x = -2
+        return str(x)
+    return ""
 
-#Exemple d'utilisation
-message = "Hello"
-print(tlv(1, message))
 
-"""
---------------------
---------------------
-"""
 def vigenere(message, key, decryption=False):
     text = ""
     key_length = len(key)
@@ -70,27 +53,39 @@ def vigenere(message, key, decryption=False):
 
     for i, char in enumerate(str(message)):
         key_index = i % key_length
-        #Letters encryption/decryption
+        # Letters encryption/decryption
         if char.isalpha():
             if decryption:
                 modified_char = chr((ord(char.upper()) - key_as_int[key_index] + 26) % 26 + ord('A'))
-            else : 
+            else:
                 modified_char = chr((ord(char.upper()) + key_as_int[key_index] - 26) % 26 + ord('A'))
-            #Put back in lower case if it was
+            # Put back in lower case if it was
             if char.islower():
                 modified_char = modified_char.lower()
             text += modified_char
-        #Digits encryption/decryption
+        # Digits encryption/decryption
         elif char.isdigit():
             if decryption:
                 modified_char = str((int(char) - key_as_int[key_index]) % 10)
-            else:  
+            else:
                 modified_char = str((int(char) + key_as_int[key_index]) % 10)
             text += modified_char
         else:
             text += char
     return text
-    
+
+
+def tlv(type, message):
+    lenght = len(message)
+    message = message.strip()
+    _tlv = "{}|{}|{}".format(type, lenght, message)
+    return _tlv
+
+
+def get_hash(string):
+    return hashing(string)
+
+
 def send_packet(key, type, content):
     """
     Envoie de données fournie en paramètres
@@ -101,8 +96,12 @@ def send_packet(key, type, content):
            (str) content:   Données à envoyer
 	:return none
     """
+    vig_cont = vigenere(content, key, decryption=False)
+    packet = tlv(type, vig_cont)
+    radio.send(packet)
 
-#Decrypt and unpack the packet received and return the fields value
+
+# Decrypt and unpack the packet received and return the fields value
 def unpack_data(encrypted_packet, key):
     """
     Déballe et déchiffre les paquets reçus via l'interface radio du micro:bit
@@ -114,9 +113,20 @@ def unpack_data(encrypted_packet, key):
             (int)lenght:           Longueur de la donnée en caractères
             (str) message:         Données reçues
     """
+    m = radio.receive()
+    if m:
+        parts = m.split("|")
+        type = parts[0]
+        lenght = parts[1]
+        message = vigenere(parts[2], key, decryption=True)
+        encrypted_packet = (type, int(lenght), message)
+        return encrypted_packet
+
+    else:
+        sleep(200)
 
 
-#Unpack the packet, check the validity and return the type, length and content
+# Unpack the packet, check the validity and return the type, length and content
 def receive_packet(packet_received, key):
     """
     Traite les paquets reçue via l'interface radio du micro:bit
@@ -129,18 +139,45 @@ def receive_packet(packet_received, key):
             (int)lenght:           Longueur de la donnée en caractère
             (str) message:         Données reçue
     """
-    
-#Calculate the challenge response
-def calculate_challenge_response(challenge):
+    if packet_received == None:
+        type = None
+        lenght = None
+        message = None
+        return (type, lenght, message)
+
+    type = packet_received[0]
+    lenght = packet_received[1]
+    message = packet_received[2]
+
+    m = hashing(message)
+    to_send = tlv(type, vigenere(m, key, decryption=False))
+    radio.send(to_send)
+
+    return (type, lenght, message)
+
+
+# Calculate the challenge response
+def calculate_challenge_response(challenge, key):
     """
     Calcule la réponse au challenge initial de connection avec l'autre micro:bit
 
     :param (str) challenge:            Challenge reçu
 	:return (srt)challenge_response:   Réponse au challenge
     """
-    
-#Ask for a new connection with a micro:bit of the same group
-def establish_connexion(key):
+    m = radio.receive()
+    if m:
+        parts = m.split("|")
+        challenge = parts[2]
+        graine = vigenere(challenge, key, decryption=True)
+        random.seed(int(graine))
+        reponse = random.randint(1, 100)
+        return str(reponse)
+    else:
+        sleep(200)
+
+
+# Ask for a new connection with a micro:bit of the same group
+def establish_connexion_Enfant(type, key):
     """
     Etablissement de la connexion avec l'autre micro:bit
     Si il y a une erreur, la valeur de retour est vide
@@ -148,28 +185,76 @@ def establish_connexion(key):
     :param (str) key:                  Clé de chiffrement
 	:return (srt)challenge_response:   Réponse au challenge
     """
+    hash_key = hashing(key)
+    vig_hash_key = vigenere(hash_key, key, decryption=False)
+    radio.send(tlv(type, vig_hash_key))
+    answer = False
+    while not answer:
+        m = radio.receive()
+        if m:
+            parts = m.split("|")
+            des_vig = vigenere(parts[2], key, decryption=True)
+            if des_vig == hash_key:
+                return 1
+
+        sleep(200)
+
+    return 0
+
 
 def main():
     return True
 
 
+##########
+# ENFANT #
+##########
 
-password = "HEISENBERG"
+# Initialisation des variables
+key = "HEISENBERG"
+hash_pass = hashing(key)
+radio.on()
+radio.config(channel=20)
+graine = None
 connexion = False
-nonce = random.randint(100,1000)
-hash_password = hashing(password)
+m = radio.receive()
+
+final_key = ""
+final_key += key
+
+# Nonce aleatoire
+nonce = random.randint(1, 100)
+nonce_str = str(nonce)
+
+# Envoi du nonce chiffre au Parent
 while not connexion:
-    vig_pass = vigenere(password, "KEYWORD", decryption=False)
-    radio.send(tlv(nonce, vig_pass))
+    display.show("?")
+    type = 1
+    send_packet(key, type, nonce_str)
     answer = False
     while not answer:
         m = radio.receive()
         if m:
-            if m == hash_password:
-                message = "ok"
-                hash_msg = hashing(message)
-                radio.send(hash_msg)
-                display.show(Image.HAPPY)
-                sleep(1000)
-                answer = True
+            parts = m.split("|")
+            p = parts[2].split("_")
+            graine = vigenere(p[0], key, decryption=True)
+            final_key += graine
+            hash_graine = hashing(graine)
+            if hash_graine == p[1]:
+                # Etablissement de la connexion
+                status = establish_connexion_Enfant(type, final_key)
+                if status == 1:
+                    display.show(Image.HAPPY)
+                    music.play(music.POWER_UP)
+                    sleep(1500)
+                    answer = True
+                else:
+                    sleep(200)
+
+        else:
+            sleep(200)
+
     connexion = True
+
+# Test
+# radio.send(final_key)
--- code/Parent_side_challenge.py
+++ code/Parent_side_challenge.py
@@ -1,108 +1,94 @@
-#from microbit import *
-#import radio
-#import random
-#import music
-#from parent_behavior import *
-
-#Can be used to filter the communication, only the ones with the same parameters will receive messages
-#radio.config(group=23, channel=2, address=0x11111111)
-#default : channel=7 (0-83), address = 0x75626974, group = 0 (0-255)
+from microbit import *
+import radio
+import random
+import music
 
 
+# Can be used to filter the communication, only the ones with the same parameters will receive messages
+# radio.config(group=23, channel=2, address=0x11111111)
+# default : channel=7 (0-83), address = 0x75626974, group = 0 (0-255)
 
-#Initialisation des variables du micro:bit
-radio.on()
-connexion_established = False
-key = "KEYWORD"
-connexion_key = None
-nonce_list = set()
-baby_state = 0
-set_volume(100)
 
 def hashing(string):
-	"""
-	Hachage d'une chaîne de caractères fournie en paramètre.
-	Le résultat est une chaîne de caractères.
-	Attention : cette technique de hachage n'est pas suffisante (hachage dit cryptographique) pour une utilisation en dehors du cours.
+    """
+    Hachage d'une chaîne de caractères fournie en paramètre.
+    Le résultat est une chaîne de caractères.
+    Attention : cette technique de hachage n'est pas suffisante (hachage dit cryptographique) pour une utilisation en dehors du cours.
 
-	:param (str) string: la chaîne de caractères à hacher
-	:return (str): le résultat du hachage
-	"""
-	def to_32(value):
-		"""
-		Fonction interne utilisée par hashing.
-		Convertit une valeur en un entier signé de 32 bits.
-		Si 'value' est un entier plus grand que 2 ** 31, il sera tronqué.
+    :param (str) string: la chaîne de caractères à hacher
+    :return (str): le résultat du hachage
+    """
 
-		:param (int) value: valeur du caractère transformé par la valeur de hachage de cette itération
-		:return (int): entier signé de 32 bits représentant 'value'
-		"""
-		value = value % (2 ** 32)
-		if value >= 2**31:
-			value = value - 2 ** 32
-		value = int(value)
-		return value
+    def to_32(value):
+        """
+        Fonction interne utilisée par hashing.
+        Convertit une valeur en un entier signé de 32 bits.
+        Si 'value' est un entier plus grand que 2 ** 31, il sera tronqué.
 
-	if string:
-		x = ord(string[0]) << 7
-		m = 1000003
-		for c in string:
-			x = to_32((x*m) ^ ord(c))
-		x ^= len(string)
-		if x == -1:
-			x = -2
-		return str(x)
-	return ""
+        :param (int) value: valeur du caractère transformé par la valeur de hachage de cette itération
+        :return (int): entier signé de 32 bits représentant 'value'
+        """
+        value = value % (2 ** 32)
+        if value >= 2 ** 31:
+            value = value - 2 ** 32
+        value = int(value)
+        return value
 
-"""
---------------------
-Ajout fonction TLV()
---------------------
-"""
-def tlv(type, message):
-    lenght = len(message)
-    message = message.strip().lower()
-    tlv = "{}|{}|{}".format(type, lenght, message)
-    return tlv
+    if string:
+        x = ord(string[0]) << 7
+        m = 1000003
+        for c in string:
+            x = to_32((x * m) ^ ord(c))
+        x ^= len(string)
+        if x == -1:
+            x = -2
+        return str(x)
+    return ""
 
 
-"""
---------------------
---------------------
-"""
-    
 def vigenere(message, key, decryption=False):
     text = ""
     key_length = len(key)
     key_as_int = [ord(k) for k in key]
 
     for i, char in enumerate(str(message)):
-        #Letters encryption/decryption
+        key_index = i % key_length
+        # Letters encryption/decryption
         if char.isalpha():
-            key_index = i % key_length
             if decryption:
                 modified_char = chr((ord(char.upper()) - key_as_int[key_index] + 26) % 26 + ord('A'))
-            else : 
+            else:
                 modified_char = chr((ord(char.upper()) + key_as_int[key_index] - 26) % 26 + ord('A'))
-            #Put back in lower case if it was
+            # Put back in lower case if it was
             if char.islower():
                 modified_char = modified_char.lower()
             text += modified_char
-        #Digits encryption/decryption
+        # Digits encryption/decryption
         elif char.isdigit():
-            key_index = i % key_length
             if decryption:
                 modified_char = str((int(char) - key_as_int[key_index]) % 10)
-            else:  
+            else:
                 modified_char = str((int(char) + key_as_int[key_index]) % 10)
             text += modified_char
         else:
             text += char
     return text
-    
+
+
+def tlv(type, message):
+    lenght = len(message)
+    message = message.strip()
+    _tlv = "{}|{}|{}".format(type, lenght, message)
+    return _tlv
+
+
+def get_hash(string):
+    return hashing(string)
+
+
 def send_packet(key, type, content):
     """
-    Envoi de données fournies en paramètres
+    Envoie de données fournie en paramètres
     Cette fonction permet de construire, de chiffrer puis d'envoyer un paquet via l'interface radio du micro:bit
 
     :param (str) key:       Clé de chiffrement
@@ -110,8 +96,12 @@ def send_packet(key, type, content):
            (str) content:   Données à envoyer
 	:return none
     """
+    vig_cont = vigenere(content, key, decryption=False)
+    packet = tlv(type, vig_cont)
+    radio.send(packet)
 
-#Unpack the packet, check the validity and return the type, length and content
+
+# Decrypt and unpack the packet received and return the fields value
 def unpack_data(encrypted_packet, key):
     """
     Déballe et déchiffre les paquets reçus via l'interface radio du micro:bit
@@ -120,14 +110,25 @@ def unpack_data(encrypted_packet, key):
     :param (str) encrypted_packet: Paquet reçu
            (str) key:              Clé de chiffrement
 	:return (srt)type:             Type de paquet
-            (int)length:           Longueur de la donnée en caractères
-            (str) message:         Données reçue
+            (int)lenght:           Longueur de la donnée en caractères
+            (str) message:         Données reçues
     """
 
+    encrypted_packet = radio.receive()
+    if encrypted_packet:
+        parts = encrypted_packet.split("|")
+        type = parts[0]
+        lenght = parts[1]
+        message = vigenere(parts[2], key, decryption=True)
+        _unpacked = (type, int(lenght), message)
+        return _unpacked
+
+
+# Unpack the packet, check the validity and return the type, length and content
 def receive_packet(packet_received, key):
     """
-    Traite les paquets reçus via l'interface radio du micro:bit
-    Cette fonction utilise la fonction unpack_data pour renvoyer les différents champs du message passé en paramètre
+    Traite les paquets reçue via l'interface radio du micro:bit
+    Cette fonction permet de construire, de chiffrer puis d'envoyer un paquet via l'interface radio du micro:bit
     Si une erreur survient, les 3 champs sont retournés vides
 
     :param (str) packet_received: Paquet reçue
@@ -136,46 +137,113 @@ def receive_packet(packet_received, key):
             (int)lenght:           Longueur de la donnée en caractère
             (str) message:         Données reçue
     """
+    if packet_received == None:
+        type = None
+        lenght = None
+        message = None
+        return (type, lenght, message)
 
-#Calculate the challenge response
-def calculate_challenge_response(challenge):
+    type = packet_received[0]
+    lenght = packet_received[1]
+    message = packet_received[2]
+
+    m = hashing(message)
+    to_send = tlv(type, vigenere(m, key, decryption=False))
+    radio.send(to_send)
+
+    # return (type, lenght, message)
+
+
+# Calculate the challenge response
+def calculate_challenge_response(challenge, key):
     """
-    Calcule la réponse au challenge initial de connection envoyé par l'autre micro:bit
+    Calcule la réponse au challenge initial de connection avec l'autre micro:bit
 
     :param (str) challenge:            Challenge reçu
 	:return (srt)challenge_response:   Réponse au challenge
     """
+    m = radio.receive()
+    if m:
+        parts = m.split("|")
+        challenge = parts[2]
+        graine = vigenere(challenge, key, decryption=True)
+        random.seed(int(graine))
+        reponse = random.randint(1, 100)
+        return str(reponse)
+    else:
+        sleep(200)
 
-#Respond to a connexion request by sending the hash value of the number received
-def respond_to_connexion_request(key):
+
+# Ask for a new connection with a micro:bit of the same group
+def establish_connexion_Parent(type, key):
     """
-    Réponse au challenge initial de connection avec l'autre micro:bit
+    Etablissement de la connexion avec l'autre micro:bit
     Si il y a une erreur, la valeur de retour est vide
 
-    :param (str) key:                   Clé de chiffrement
-	:return (srt) challenge_response:   Réponse au challenge
+    :param (str) key:                  Clé de chiffrement
+	:return (srt)challenge_response:   Réponse au challenge
     """
+    hash_key = hashing(key)
+    answer = False
+    while not answer:
+        m = radio.receive()
+        if m:
+            parts = m.split("|")
+            des_vig = vigenere(parts[2], key, decryption=True)
+            if des_vig == hash_key:
+                radio.send(tlv(type, vigenere(hash_key, key, decryption=False)))
+                return 1
+    return 0
+
 
 def main():
     return True
 
 
+##########
+# PARENT #
+##########
 
-password = "HEISENBERG"
-verification = "ok"
-hash_ver = hashing(verification)
+
+# Initialisation des variables
+key = "HEISENBERG"
+hash_pass = hashing(key)
+radio.on()
+radio.config(channel=20)
 connexion = False
+graine = None
+hash_graine = None
+m = radio.receive()
+
+final_key = ""
+final_key += key
+
+# Recuperer nonce + set seed
 while not connexion:
-    m = radio.receive()
-    if m:
-        parts = m.split("|")
-        des_vig = vigenere(parts[2], key, decryption=True)
-        radio.send(hashing(des_vig))
-        answer = False
-        while not answer:
-            m = radio.receive()
-            if m:
-                if m == hash_ver:
-                    display.show(Image.HAPPY)
-                    answer = True
-        connexion = True
+    type = 1
+    display.show("?")
+    result = calculate_challenge_response(m, key)
+    if result != None:
+        # Configuration de la graine locale
+        graine = int(result)
+        hash_graine = hashing(result)
+        vig_graine = vigenere(result, key, decryption=False)
+        final_key += result
+        # Configuration de la graine a distance
+        message = vig_graine + "_" + hash_graine
+        radio.send(tlv(type, message))
+        # Etablissement de la connexion
+        status = establish_connexion_Parent(type, final_key)
+        if status == 1:
+            display.show(Image.HAPPY)
+            music.play(music.POWER_UP)
+            sleep(1500)
+            connexion = True
+        else:
+            sleep(200)
+
+    else:
+        sleep(200)
+
+# Test
+# radio.send(final_key)
